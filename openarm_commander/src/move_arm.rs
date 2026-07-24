580c447f8f36ade194142a044711905f8497e1f5
Jared Muirhead <jared@peppy.bot>
2026-07-24 10:24:12 -0700

    commander: rustfmt over the envelope migration

--- openarm_commander/src/move_arm.rs
+++ openarm_commander/src/move_arm.rs
@@ -89,9 +89,7 @@ async fn run(
     {
         Ok(handle) if handle.accepted => handle,
         Ok(handle) => {
-            let reason = handle
-                .reason
-                .unwrap_or_else(|| "no reason given".into());
+            let reason = handle.reason.unwrap_or_else(|| "no reason given".into());
             finalize(
                 &feedback,
                 side,
